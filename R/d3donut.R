26ba639f14aa516bf49a82787c363c4ed246f8a4
Matthew Brierley <Matthew.Brierley@novartis.com>
2018-05-09 18:29:25 +0200

    initial commit

---
+++ R/d3donut.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+d3donut <- function(message, width = NULL, height = NULL, elementId = NULL) {
+  
+  # forward options using x
+  x = list(
+    message = message
+  )
+  
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'd3donut',
+    x,
+    width = width,
+    height = height,
+    package = 'd3donut',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for testD3
+#'
+#' Output and render functions for using testD3 within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a testD3
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name d3donut-shiny
+#'
+#' @export
+d3donutOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'd3donut', width, height, package = 'd3donut')
+}
+
+#' @rdname d3donut-shiny
+#' @export
+renderD3donut <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, d3donutOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/d3donut.js
@@ -0,0 +1,170 @@
+
+      
+      var width = 600;
+			var height = 650;
+			var radius = Math.min(width, height) / 2;
+			var donutWidth = 150;
+			
+			// Set up a vector with the colours to use
+			// var color = d3.scaleOrdinal(d3.schemeCategory20b);
+			var sectorcolors = ["#fdbe85", "#bdd7e7", "#bae4b3"];
+			var sectorcolorshighlighted = ["#a63603", "#08519c", "#006d2c"];
+			var sectors      = ["tm_da", "trial_code", "trial_biostatistician"];
+			
+      
+  		// Set up the svg object centered on the middle of the object
+			var svg = d3.select('#d3donutchart')
+				.append('svg')
+				.attr('width', width)
+				.attr('height', height)
+				.append('g')
+				.attr('transform', 'translate(' + (width/2) + ',' + (height / 2) + ')');
+				
+			// Set up an arc object with predefined inner & outer radius
+			var arc = d3.arc()
+				.innerRadius(radius - donutWidth)
+				.outerRadius(radius);
+			
+			// Set up an unsorted pie object
+			var pie = d3.pie()
+				.value(function (d) {return d.weight;})
+				.sort(null);
+				
+			// Load in the dataset
+			d3.csv('donut2.csv', function(error, dataset) {
+			
+				// Convert count variable from character to numeric
+				dataset.forEach (function (d) {
+					d.weight = +d.weight;
+				});
+
+				// Draw the segments of the pie using the pie and arc objects
+				var path = svg.selectAll()                                                               // Select all tags in the object called 'svg'
+					.data(pie(dataset))                                                                  // Attach the data
+					.enter()
+					.append('path')	
+					.attr('id', function(d) {return d.data.id})
+					.attr('class', function(d) {return d.data.sector + " " + d.data.links})
+					.attr('d', arc)
+					.attr('stroke', 'grey')
+					.attr('fill', function(d) {
+						return sectorcolors[sectors.indexOf(d.data.sector)];
+					})
+                 	.on('mouseenter', function(d) {
+
+
+                        var segmentselected = false;
+
+                        d3.selectAll("path").each (function (d) {
+                            if (d3.select(this).attr("class").indexOf("clicked") != -1) {
+                                segmentselected = true;
+                            }
+                        })
+
+                        if (segmentselected == false) {
+
+                            // Change the background fill colour
+                            d3.select(this).attr('fill', 'purple');
+                            currID = d3.select(this).attr("id");
+                            for (i = 0; i < sectors.length; i++) {
+                                svg.selectAll("." + sectors[i]).filter("." + currID)
+                                    .attr('fill', sectorcolorshighlighted[i]);
+                                };
+
+                            // Add the connecting lines
+                            startCurve = (radius-donutWidth)*Math.cos((Math.PI/2) - 0.5*(d.startAngle + d.endAngle)) + "," +
+                                         -(radius-donutWidth)*Math.sin((Math.PI/2) - 0.5*(d.startAngle + d.endAngle));
+
+                            var lines = svg.selectAll()
+                                 .data(pie(dataset))
+                                 .enter()
+                                 .append('path')
+                                 .attr('class', 'connectingline')
+                                 .attr('fill', 'none')
+                                 .attr('stroke', 'black')
+                                 .attr('d', function(d) {
+                                    endCurve = (radius-donutWidth)*Math.cos((Math.PI/2) - 0.5*(d.startAngle + d.endAngle)) + "," +
+                                               -(radius-donutWidth)*Math.sin((Math.PI/2) - 0.5*(d.startAngle + d.endAngle));
+                                    if (d.data.links.indexOf(currID) != -1) {return "M" + startCurve + " Q0,0 " + endCurve;}
+                                  });
+
+                            var dots = svg.selectAll()
+                                .data(pie(dataset))
+                                .enter()
+                                .append('circle')
+                                .attr('class', 'connectingdot')
+                                .attr('cx', function(d) {if (d.data.id.indexOf(currID) != -1 | d.data.links.indexOf(currID) != -1) {return (radius-donutWidth)*Math.cos((Math.PI/2) - 0.5*(d.startAngle + d.endAngle));}})
+                                .attr('cy', function(d) {if (d.data.id.indexOf(currID) != -1 | d.data.links.indexOf(currID) != -1) {return  -(radius-donutWidth)*Math.sin((Math.PI/2) - 0.5*(d.startAngle + d.endAngle));}})
+                                .attr('r', function(d) {if (d.data.id.indexOf(currID) != -1 | d.data.links.indexOf(currID) != -1) {return '3';}});
+
+
+                        }
+
+
+
+						})
+					.on('mouseleave', function(d) {
+                        var segmentselected = false;
+
+                        d3.selectAll("path").each (function (d) {
+                            if (d3.select(this).attr("class").indexOf("clicked") != -1) {
+                                segmentselected = true;
+                            }
+                        })
+
+
+                        if (segmentselected == false) {
+                            for (i = 0; i < sectors.length; i++) {
+                                svg.selectAll("." + sectors[i])
+                                .attr('fill', sectorcolors[i]);
+							    };
+                                d3.selectAll("path.connectingline").remove();
+                                d3.selectAll("circle.connectingdot").remove();
+						    }
+                        })
+                    .on('click', function(d) {
+
+                        var selectedsegmentID = "";
+
+                        d3.selectAll("path").each(function (d) {
+                            if (d3.select(this).attr("class").indexOf("clicked") != -1) {
+                                selectedsegmentID = d3.select(this).attr("id");
+                            }
+                            });
+
+                        if (selectedsegmentID == "" | selectedsegmentID == d3.select(this).attr("id")) {
+                            if (d3.select(this).classed('clicked') == true) {
+                                d3.select(this).classed('clicked', false);
+                            } else {
+                                d3.select(this).classed('clicked', true);
+                            }
+                        }
+
+                        });
+
+  			
+				var legend = svg.selectAll()	
+					.data(pie(dataset))
+					.enter()
+					.append('text')
+					.attr('transform', function(d) {
+						return "translate(" + arc.centroid(d) + 
+						       ") rotate(" + angle(d) + ")";	
+					})
+				    .attr('text-anchor', "middle")
+					.attr('pointer-events', "none")
+                    .attr('style', "font-family: Arial; font-size: 12px")
+					.text(function(d) {return d.data.name;});
+				
+			});
+			
+			
+			
+			
+			
+			// Computes the angle of an arc, converting from radians to degrees.
+			function angle(d) {
+				var a = (d.startAngle + d.endAngle) * 90 / Math.PI - 90;
+				return a > 90 ? a - 180 : a;
+			}
+			
